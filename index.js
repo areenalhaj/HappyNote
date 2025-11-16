eed05a0a26d52f8f608978bfb901932b8206c47a
areenalhaj <arenalhaj@gmail.com>
2025-11-16 02:03:56 +0200

    adding some stiles

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ var counter = 0;
 var c = 0;
 var tracker = document.getElementsByClassName("add-btn")[0].addEventListener('click', function () {
     var taskData = document.getElementById("new-task").value;
-    if (taskData !== "") {
-        notes.push(`<li class="note${(counter + 1)}">${taskData} <button id="delete-btn">Delete</button></li>`);
+    if (taskData !== "" && taskData !== undefined) {
+        notes.push(`<li class="note${counter}">${taskData}<button class="btns texts delete-btn">delete</button></li>`);
+        // inserting the note
         document.getElementsByClassName("tasks-list")[0].insertAdjacentHTML('afterbegin', notes[counter]);
         document.getElementById("new-task").value = "";
+        // delete button function:
+        document.querySelector(`.note${counter}>button`).addEventListener('click', function () {
+            this.parentElement.remove();
+        })
+        counter++;
     }
-    counter++;
 });
-
-document.getElementById("delete-btn").addEventListener('click', function () {
-    document.getElementsByClassName("tasks-list")[0].getElementsByTagName("li")[counter - 1].remove();
-    counter--;
-}
-)
-
